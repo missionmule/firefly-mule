b9980e6212972da7b3d691d566d45fbd7bd58b06
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-07-01 18:09:16 -0700

    Use INFO log level in production

--- avionics/__main__.py
+++ avionics/__main__.py
@@ -26,14 +26,14 @@ def setup_logging():
         logging_level = logging.INFO
 
     logging.basicConfig(filename='flight.log',
-                        level=logging.DEBUG,
+                        level=logging_level,
                         format='%(asctime)s.%(msecs)03d %(levelname)s \t%(message)s',
                         datefmt="%d %b %Y %H:%M:%S")
 
     # Log to STDOUT
     # TODO: only log to stdout in debug mode to speed things up
     ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
+    ch.setLevel(logging_level)
     formatter = logging.Formatter('%(asctime)s.%(msecs)03d %(levelname)s %(message)s')
     ch.setFormatter(formatter)
     logging.getLogger().addHandler(ch)
