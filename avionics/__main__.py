1eeaf64469c6b3aa6591bce77e0b83f95d9946ea
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-06 15:07:59 -0700

    Increased download timeout to 10 mins

--- avionics/__main__.py
+++ avionics/__main__.py
@@ -66,7 +66,10 @@ def main():
     services.append(ser)
 
     # Data station communication handling
-    dl = DataStationHandler(20000, 20000, 60000, ser.rx_queue)
+    # 20 sec. connection timeout
+    # 20 sec. read/write timeout
+    # 10 min. download timeout
+    dl = DataStationHandler(20000, 20000, 600000, ser.rx_queue)
     services.append(dl)
 
     # Heartbeat pushed to serial tx_queue every 500ms
